1af5e3d4a7a248ae238ce0a46be7243eb1a8f573
mentalK94 <doingnow94@gmail.com>
2020-06-27 03:18:10 +0900

    Review Model 작성

---
+++ src/main/java/kr/co/mentalK94/withus/domains/Review.java
@@ -0,0 +1,30 @@
+package kr.co.mentalK94.withus.domains;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Review {
+
+    private Long id; // review id
+
+    private Long productId; // product id
+
+    private String writer; // 작성자
+
+    private float rating; // 평점
+
+    private String description; // 리뷰 내용
+
+    private String creationDate; // 작성일
+
+    private String photo1; // 상품 리뷰 사진 1
+
+    private String photo2; // 상품 리뷰 사진 2
+
+    private String photo3; // 상품 리뷰 사진 3
+
+}
