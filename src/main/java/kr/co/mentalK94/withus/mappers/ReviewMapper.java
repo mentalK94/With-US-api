6899fb6e233425652677316e46720c1d4edf69bb
mentalK94 <doingnow94@gmail.com>
2020-06-27 04:13:59 +0900

    @Mapper 어노테이션 추가 및 포스트맨 테스트 완료

--- src/main/java/kr/co/mentalK94/withus/mappers/ReviewMapper.java
+++ src/main/java/kr/co/mentalK94/withus/mappers/ReviewMapper.java
@@ -1,8 +1,10 @@
 package kr.co.mentalK94.withus.mappers;
 
 import kr.co.mentalK94.withus.domains.Review;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+@Mapper
 @Repository
 public interface ReviewMapper {
 
