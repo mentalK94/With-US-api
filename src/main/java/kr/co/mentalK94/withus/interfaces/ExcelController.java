d6796903cbaa2b9bf87121b857d394348f1b265f
mentalK94 <doingnow94@gmail.com>
2020-06-30 11:20:43 +0900

    Excel Download 기능을 위한 gradle dependency 추가 및 ExcelGeneratorUtil, ExcelController 구현

---
+++ src/main/java/kr/co/mentalK94/withus/interfaces/ExcelController.java
@@ -0,0 +1,12 @@
+package kr.co.mentalK94.withus.interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class ExcelController {
+
+    @GetMapping("/downloadExcelFile")
+    public
+}
---
+++ src/main/java/kr/co/mentalK94/withus/utils/ExcelGeneratorUtil.java
@@ -0,0 +1,63 @@
+package kr.co.mentalK94.withus.utils;
+
+import kr.co.mentalK94.withus.domains.Purchase;
+import kr.co.mentalK94.withus.domains.PurchaseItem;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class ExcelGeneratorUtil {
+
+    // 구매내역 -> 엑셀파일 다운로드
+    public static ByteArrayInputStream purchaseHistoryToExcel(List<Purchase> purchaseList, String username) throws IOException {
+        String[] Columns = {"구매번호", "구매상품 명", "총 금액", "사용 포인트", "결제 금액", "주문자", "배송지", "배송 메모", "연락처", "구매일시"};
+
+        try(
+                Workbook workbook = new XSSFWorkbook();
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+         ) {
+            CreationHelper creationHelper = workbook.getCreationHelper();
+
+            Sheet sheet = workbook.createSheet("purchaseHistory");
+
+            Font headerFont = workbook.createFont();
+            headerFont.setBold(true);
+
+            CellStyle headerCellStyle = workbook.createCellStyle();
+            headerCellStyle.setFont(headerFont);
+
+            Row headerRow = sheet.createRow(0);
+
+            for(int col=0; col<Columns.length; col++) {
+                Cell cell = headerRow.createCell(col);
+                cell.setCellValue(Columns[col]);
+                cell.setCellStyle(headerCellStyle);
+            }
+
+            int rowIdx = 1;
+            for(Purchase purchase : purchaseList) {
+                Row row = sheet.createRow(rowIdx++);
+
+                row.createCell(0).setCellValue(purchase.getId());
+                if(purchase.getPurchaseItems().size() == 1) {
+                    row.createCell(1).setCellValue(purchase.getPurchaseItems().get(0).getProduct().getName());
+                } else {
+                    row.createCell(1).setCellValue(purchase.getPurchaseItems().get(0).getProduct().getName()
+                            + "외 " + (purchase.getPurchaseItems().size()-1) +"개 상품");
+                }
+                row.createCell(2).setCellValue(purchase.getTotalPrice());
+                row.createCell(3).setCellValue(purchase.getUsingPoint());
+                row.createCell(4).setCellValue(purchase.getPayAmount());
+                row.createCell(5).setCellValue(username);
+                row.createCell(6).setCellValue(purchase.getShippingAddress());
+                row.createCell(7).setCellValue(purchase.getShippingMemo());
+                row.createCell(8).setCellValue(purchase.getPhone());
+                row.createCell(9).setCellValue(purchase.getDateTime());
+            }
+        }
+    }
+}
