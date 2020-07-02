3f979187df0e735a95ece147ffb231984bc03802
mentalK94 <doingnow94@gmail.com>
2020-07-02 17:35:28 +0900

    결제 후 처리 기능 구현 - 재고 갱신

--- src/main/java/kr/co/mentalK94/withus/mappers/ProductMapper.java
+++ src/main/java/kr/co/mentalK94/withus/mappers/ProductMapper.java
@@ -15,9 +15,13 @@ public interface ProductMapper {
 
     Product selectProduct(Long id) throws Exception;
 
+    int selectProductStock(Long id);
+
     void insertProduct(Product product);
 
     void updateProduct(@Param("product") Product product, @Param("id")Long id);
+
+    void updateProductStock(@Param("productId")Long id, @Param("stock")int stock);
 
     void imageUpdate(String imageFileName, @Param("id")Long id);
 
