3f1c7e9992890d6b5357b292133750ae5da0cd19
mentalK94 <doingnow94@gmail.com>
2020-07-02 18:29:28 +0900

    평점계산 Util - 소수점 2자리에서 반올림 코드 추가

--- src/main/java/kr/co/mentalK94/withus/utils/RatingCalculationUtil.java
+++ src/main/java/kr/co/mentalK94/withus/utils/RatingCalculationUtil.java
@@ -5,7 +5,7 @@ public class RatingCalculationUtil {
     public static double calculate(int count, double existingRating, double addRating) {
         double calculatedRating = 0;
 
-        calculatedRating = ((count * existingRating) + addRating) / (count+1);
+        calculatedRating = Double.parseDouble(String.format("%.2f", ((count * existingRating) + addRating) / (count+1)));
 
         return calculatedRating;
     }
