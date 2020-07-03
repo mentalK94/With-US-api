999b56416c6203b1e3797af40c7be83d7d0f497b
mentalK94 <doingnow94@gmail.com>
2020-07-03 16:42:26 +0900

    Javamail - MailUtil 구현

---
+++ src/main/java/kr/co/mentalK94/withus/utils/MailUtil.java
@@ -0,0 +1,46 @@
+package kr.co.mentalK94.withus.utils;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.activation.DataSource;
+import java.io.UnsupportedEncodingException;
+
+public class MailUtil {
+
+    private JavaMailSender javaMailSender;
+    private MimeMessage message;
+    private MimeMessageHelper messageHelper;
+
+    public MailUtil(JavaMailSender javaMailSender) throws MessagingException {
+        this.javaMailSender = javaMailSender;
+        this.message = this.javaMailSender.createMimeMessage();
+        this.messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+    }
+
+    public void setSubject(String subject) throws MessagingException {
+        messageHelper.setSubject(subject);
+    }
+
+    public void setText(String htmlContent) throws MessagingException {
+        messageHelper.setText(htmlContent, true);
+    }
+
+    public void setFrom(String email, String name) throws UnsupportedEncodingException, MessagingException {
+        messageHelper.setFrom(email, name);
+    }
+
+    public void setTo(String email) throws MessagingException {
+        messageHelper.setTo(email);
+    }
+
+    public void addInline(String contentId, DataSource dataSource) throws MessagingException {
+        messageHelper.addInline(contentId, dataSource);
+    }
+
+    public void send() {
+        javaMailSender.send(message);
+    }
+}
